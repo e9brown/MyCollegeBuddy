46ddf0b9e09081a9731985fa328e189058877af4
Alexander Louden <alouden@ucsd.edu>
2016-02-04 11:52:58 -0800

    Renaming variables

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,11 @@
  */
 exports.view = function(req, res){
    res.render('index', {
-    'projects': [
+    'buttons': [
       { 'name': 'Add Experience',
-        //'image': 'lorempixel.people.1.jpeg',
         'id': 'option1'
       },
       { 'name': 'Experience History',
-        //'image': 'lorempixel.city.1.jpeg',
         'id': 'option2'
       },
     ]  
--- routes/project.js
+++
@@ -1,6 +0,0 @@
-exports.viewProject = function(req, res) {
-   res.render('project'); 
-   // controller code goes here?
-  var name = req.params.name;
-  console.log("The project name is: " + name);
-};
