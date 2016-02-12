cfbb5002f99af04326fec6872e020d5934a82129
Erik Brown <e9brown@ucsd.edu>
2016-02-12 03:37:29 +0000

    basic add is working

--- routes/add.js
+++ routes/add.js
@@ -2,10 +2,12 @@ var data = require('../data.json');
 
 exports.viewAdd = function(req, res) {
    var newEntry = {
-		'title' :req.query.name,
+		'title' :req.query.title,
 	   	'date' :req.query.date,
 	   	'edit' :req.query.date,
 	   	'category' :req.query.category,
 	   }	
+   console.log(newEntry);
+   data["entries"].push(newEntry);
    res.render("add", {'data':data});
 };
