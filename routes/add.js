c8b637886eea69e7d1ffc59be94e049a78899923
Alexander Louden <alouden@ucsd.edu>
2016-02-11 21:48:31 -0800

    Required title and category for add.

--- routes/add.js
+++ routes/add.js
@@ -1,14 +1,43 @@
+'use strict';
+
+
 var data = require('../data.json');
 
 exports.viewAdd = function(req, res) {
-   var newEntry = {
+   //console.log(req.query);
+    var newEntry = {
 		'title' :req.query.title,
 	   	'date' :req.query.date,
 	   	'edit' :req.query.date,
 	   	'category' :req.query.category,
 		'description' :req.query.description
 	   }	
-   console.log(newEntry);
-   data["entries"].push(newEntry);
-   res.render("add", {'data':data});
+    //console.log(newEntry);
+    console.log(validateForm(newEntry));
+    if( validateForm(newEntry) ){
+   		data["entries"].push(newEntry);
+   	}
+   	res.render("add", {'data':data});
 };
+
+function validateForm(entry) {
+    if (entry.title == "" || entry.title == null
+    	|| entry.category == "" || entry.category == null){
+    	console.log("Title and Category are required fields.")
+    	//$(".alert").html('<p>The field above is required to submit. </p>');
+    	//validateMessage();
+    	return false;
+    }
+    else return true;
+};
+
+/* $(document).read(function(){
+	intializePage();
+})
+function initialzePage(){
+	$('.submit').click(validateMessage);
+} */
+
+/*function validateMessage(){
+	$(".alert").html('<p>The field above is required to submit. </p>');
+}*/
--- routes/history.js
+++ routes/history.js
@@ -3,6 +3,7 @@ var data = require("../data.json");
  * GET home page.
  */
 exports.viewHistory = function(req, res){
+   console.log(data.entries);	
    res.render('history',{'data':data});
   
 };
