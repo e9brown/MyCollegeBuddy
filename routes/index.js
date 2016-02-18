fd4f6970952b354b4407b664f9f70c228f2361ec
Eamon Brennan <ewbrenna@ucsd.edu>
2016-02-18 12:07:12 +0000

    moved fb uid to js file

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,28 @@ exports.view = function(req, res){
    res.render('index', {'data':data});
 };
 
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '759982734106669',
+      xfbml      : true,
+      version    : 'v2.5'
+    });
+	var data = require('../data.json');
+	FB.getLoginStatus(function(response) {
+  if (response.status === 'connected') {
+	var uid = response.authResponse.userID;
+	console.log("JS FB UID: " + uid);
+	var newuid = {'uid' : uid}
+	data["fbuid"].push(newuid);
+	var accessToken = response.authResponse.accessToken;
+  }
+ });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
