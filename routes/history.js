c436d23b70915021de380d952336a55d2199e95b
Alexander Louden <alouden@ucsd.edu>
2016-02-18 21:58:14 -0800

    Previous entries page sorted by category.

--- routes/history.js
+++ routes/history.js
@@ -3,7 +3,20 @@ var data = require("../data.json");
  * GET home page.
  */
 exports.viewHistory = function(req, res){
-   console.log(data.entries);	
+   //console.log(data.entries);	
+   data["entries-sorted"] = [];
+   for (cat in data.categories){
+   	//console.log("HEREERERE")
+
+   	//console.log(data.categories[cat]);
+   		for (var ent in data.entries){
+   			console.log(data.categories[ent]);
+   			console.log("NewEntry")
+   			if (data.categories[cat].category == data.entries[ent].category){
+   				//console.log("HEREERERE")
+   				data["entries-sorted"].push(data.entries[ent]);
+   			}
+   		}
+   }
    res.render('history',{'data':data});
-  
 };
